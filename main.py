82f947298a023aa263407a05e154155c8969899d
CarlMartinL <146339918+CarlMartinL@users.noreply.github.com>
2025-05-16 10:43:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 """
--Make the chatbot remember previous messages (+3)
--Highlight whos message is whose. (Carl-GPT or User) (+2)
--Launch on the actual internet (+3)
--Connect to personal domain(+2)
--Make visible for google search using keywords and website description (+3)
+Degree of difficlulty points in readme.py
 """
 
 
