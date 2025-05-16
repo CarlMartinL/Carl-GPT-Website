d307efe0a4870fb54bb96e70d785e0ed5c0142a8
CarlMartinL <146339918+CarlMartinL@users.noreply.github.com>
2025-05-16 10:43:25 -0500

    Update readme.py

--- readme.py
+++ readme.py
@@ -1,1 +1,9 @@
+"""
+Degree of difficlulty points:
 
+-Make the chatbot remember previous messages (+3)
+-Highlight whos message is whose. (Carl-GPT or User) (+2)
+-Launch on the actual internet (+3)
+-Connect to personal domain(+2)
+-Make visible for google search using keywords and website description (+3)
+"""
