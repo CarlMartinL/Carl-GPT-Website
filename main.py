ea57fb5401471fcf2d5ba44d25bcad7530a69824
CarlMartinL <146339918+CarlMartinL@users.noreply.github.com>
2025-05-16 08:39:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+-Make the chatbot remember previous messages (+3)
+-Highlight whos message is whose. (Carl-GPT or User) (+2)
+-Launch on the actual internet (+3)
+-Connect to personal domain(+2)
+-Make visible for google search using keywords and website description (+3)
+"""
+
+
 from website import create_app
 
 app = create_app()
